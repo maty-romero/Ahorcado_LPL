40ac5053447b8dcd13cf7cb3c0e26b6750fb6c0c
Hans <ich.arbeite.rd>
2024-02-11 22:56:18 -0300

    format view resultado

--- resources/views/resultado.blade.php
+++ resources/views/resultado.blade.php
@@ -44,12 +44,40 @@
             @endif
         @endif
 
+        <x-modal>
+            <x-slot name="idModal">modalDificultad</x-slot>
+            <x-slot name="titulo">Elecci&oacute;n dificultad</x-slot>
+            
+            <div class="form-group" id="arriesgaConteiner">
+                <label for="txtPalabra" class="font-weight-bold" style="font-size: 1.3em;">Seleccione una dificultad:</label>
+                <select class="form-control" id="cmbDificultad" name="dificultad">
+                    <option value="1">Baja</option>
+                    <option value="2">Media</option>
+                    <option value="3">Alta</option>
+                </select>
+            </div>
+    
+            <x-slot name="botonFooter">
+                <button id="btnDificultad" type="button" class="btn btn-warning">Jugar</button>
+            </x-slot>
+        </x-modal>
+    
+        <script>
+            document.getElementById('btnDificultad').addEventListener('click', function() {
+                document.getElementById('idDificultadForm').value = document.getElementById("cmbDificultad").value;;
+                document.getElementById('formEleccionDificultad').submit();
+            });
+        </script>
+
         <div class="text-center pt-3">
-            <a href="{{route('home')}}" class="btn btn-primary mr-2" style="font-size: 1.4em;">Jugar nueva partida</a>
+            <button type="button" class="btn btn-primary btn-lg mr-4" data-bs-toggle="modal" data-bs-target="#modalDificultad">Juego nuevo</button>
             <a href="{{route('home')}}" class="btn btn-secondary mr-2" style="font-size: 1.4em;">Volver al Inicio</a>
 
         </div>
     </div>
 
+    <form id="formEleccionDificultad" method="POST" action="{{ route('inicioPartida') }}">
+        <input type="hidden" name="dificultad" id="idDificultadForm">
+    </form>
 
 @endsection
